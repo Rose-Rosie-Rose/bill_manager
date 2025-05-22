a36a80e021293ae73ce78d55d29b2dcfdd4e0e33
Rosie-Rose-Rosie <88028208+Rose-Rosie-Rose@users.noreply.github.com>
2025-05-22 19:40:47 +0900

    feat: set up base layout for main page before component separation

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,19 @@
 export const HomePage = () => {
-  return <>
-  
-  </>;
+  return (
+    <div>
+      <div>
+        <h1>미납정리</h1>
+      </div>
+      <div>
+        <div>은행 선택 드롭박스</div>
+        <div>
+          <div>은행 입금내역 엑셀 시트 업로드</div>
+        </div>
+        <div>청구서 pdf 파일 업로드</div>
+      </div>
+      <div>
+        <div>미납 내역 정리 table</div>
+      </div>
+    </div>
+  );
 };
